bd7214f84a5d19eabeef9b45eb51095923ad07b1
kenfav <kenfavero@gmail.com>
2021-05-30 08:22:54 -0400

    kv file modified to add boxlayout for summary at the end

---
+++ RelatoryModel.py
@@ -0,0 +1,12 @@
+class RelatoryModel:
+    def connect(self, filename):
+        pass
+        
+    def create_schema(self):
+        pass
+    
+    def insert_preaching_activity(self, data, horas, video, revisitas, publicacoes):
+        pass
+    
+    def monthly_activity(self, month):
+        return [...]
